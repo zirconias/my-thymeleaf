007df423effdcaca74793100e439b7dc62bacf70
islam <zirconias777@yahoo.com>
2015-07-10 17:55:15 +0100

    before adding spring security

--- src/main/java/com/zirconias/thyspring/signup/SignupController.java
+++ src/main/java/com/zirconias/thyspring/signup/SignupController.java
@@ -3,8 +3,12 @@ package com.zirconias.thyspring.signup;
 import com.zirconias.thyspring.AjaxUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 /**
  * Created by xirc on 09/07/2015.
@@ -22,4 +26,18 @@ public class SignupController {
         return "signup/signup";
     }
 
+    @RequestMapping(value = "signin")
+    public String signin() {
+        return "signin/login";
+    }
+
+    @RequestMapping(value = "signin", method = RequestMethod.POST)
+    public String signup(@RequestParam String j_username,@RequestParam String j_password) {
+
+        //Account account = accountRepository.save(signupForm.createAccount());
+        //userService.signin(account);
+        // see /WEB-INF/i18n/messages.properties and /WEB-INF/views/homeSignedIn.html
+        //MessageHelper.addSuccessAttribute(ra, "signup.success");
+        return "redirect:/";
+    }
 }
